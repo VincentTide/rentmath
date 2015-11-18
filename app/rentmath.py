a284e8708353b77fbf4b9cced2667af870002336
Vincent Zhang <vincenttide@gmail.com>
2015-11-18 18:52:54 -0500

    work

--- app/rentmath.py
+++ app/rentmath.py
@@ -1,7 +1,30 @@
+from decimal import Decimal
 
 
 
-def calc_rental(data):
+def float_to_percent(num):
+    d = Decimal(num*100).quantize(Decimal('.01'))
+    return d
+
+
+def float_to_currency(num):
+    '''
+
+    :param num: float
+    :return: currency formatted string
+    '''
+    # truncate float to 2 decimal places
+    d = Decimal(num).quantize(Decimal('.01'))
+    # add comma for thousands separator
+    d = "{:,.2f}".format(d)
+    return d
+
+def integer_to_currency(num):
+    d = "{:,d}".format(num)
+    return d
+
+
+def do_rental_math(data):
     project_cost = data['purchase_price'] + data['closing_cost'] + data['repair_cost']
     down_payment_amount = data['purchase_price'] * (data['down_payment_percent']/100.0)
     loan_amount = data['purchase_price'] - down_payment_amount
@@ -17,33 +40,38 @@ def calc_rental(data):
     loan_payment = (r*P * pow(1+r, N)) / (pow(1+r, N) - 1)
 
     cash_needed = down_payment_amount + data['closing_cost'] + data['repair_cost']
-    rent = data['rental_income_monthly']
+    misc_income = data['misc_income_monthly']
+    income = data['rental_income_monthly'] + misc_income
+
 
     # Expenses other than loan
     property_tax = data['property_tax_annual'] / 12.0
     hazard_insurance = data['hazard_insurance_annual'] / 12.0
     flood_insurance = data['flood_insurance_annual'] / 12.0
-    utilities = rent * (data['utilities_monthly_percent'] / 100.0)
-    vacancy = rent * (data['vacancy_monthly_percent'] / 100.0)
-    repairs = rent * (data['repairs_monthly_percent'] / 100.0)
-    capex = rent * (data['capex_monthly_percent'] / 100.0)
-    management = rent * (data['manager_monthly_percent'] / 100.0)
+    utilities = income * (data['utilities_monthly_percent'] / 100.0)
+    vacancy = income * (data['vacancy_monthly_percent'] / 100.0)
+    repairs = income * (data['repairs_monthly_percent'] / 100.0)
+    capex = income * (data['capex_monthly_percent'] / 100.0)
+    management = income * (data['manager_monthly_percent'] / 100.0)
+    hoa = data['hoa_monthly']
+    misc_expense = data['misc_expense_monthly']
 
     expenses_total = loan_payment + property_tax + hazard_insurance + flood_insurance +\
-        utilities + vacancy + repairs + capex + management
+        utilities + vacancy + repairs + capex + management + hoa + misc_expense
     expenses_no_mortgage = property_tax + hazard_insurance + flood_insurance +\
-        utilities + vacancy + repairs + capex + management
+        utilities + vacancy + repairs + capex + management + hoa + misc_expense
 
     # Cash Flow Monthly
-    cash_flow = rent - expenses_total
+    cash_flow = income - expenses_total
     cash_flow_per_unit = cash_flow / data['units_in_property']
-    # Net Operating Income Yearly
-    net_operating_income = (rent - expenses_no_mortgage) * 12
+    # Net Operating Income Monthly
+    net_operating_income = (income - expenses_no_mortgage)
     # Cap Rate - purchase price + closing cost + repairs
-    cap_rate = net_operating_income / project_cost
+    cap_rate = (net_operating_income * 12) / project_cost
     cash_on_cash_return = (cash_flow * 12) / cash_needed
 
     result = {
+        'property_name': data['property_name'],
         'purchase_price': data['purchase_price'],
         'closing_cost': data['closing_cost'],
         'repair_cost': data['repair_cost'],
@@ -54,16 +82,25 @@ def calc_rental(data):
         'loan_interest_rate_percent': data['loan_interest_rate_percent'],
         'loan_period_years': data['loan_period_years'],
         'cash_needed': cash_needed,
-        'rent': rent,
+        'rental_income': data['rental_income_monthly'],
+        'misc_income': misc_income,
+        'total_income': income,
         'loan_payment': loan_payment,
         'property_tax': property_tax,
         'hazard_insurance': hazard_insurance,
         'flood_insurance': flood_insurance,
         'utilities': utilities,
+        'utilities_percent': data['utilities_monthly_percent'],
         'vacancy': vacancy,
+        'vacancy_percent': data['vacancy_monthly_percent'],
         'repairs': repairs,
+        'repairs_percent': data['repairs_monthly_percent'],
         'capex': capex,
+        'capex_percent': data['capex_monthly_percent'],
         'management': management,
+        'management_percent': data['manager_monthly_percent'],
+        'hoa': hoa,
+        'misc_expense': misc_expense,
         'expenses_total': expenses_total,
         'expenses_no_mortgage': expenses_no_mortgage,
         'cash_flow': cash_flow,
@@ -74,6 +111,73 @@ def calc_rental(data):
     }
 
     return result
+
+
+def rental_main(data):
+    r = do_rental_math(data)
+
+    # Create dict to float currency format
+    currency = {
+        'down_payment_amount': r['down_payment_amount'],
+        'loan_amount': r['loan_amount'],
+        'cash_needed': r['cash_needed'],
+        'rental_income': r['rental_income'],
+        'misc_income': r['misc_income'],
+        'total_income': r['total_income'],
+        'loan_payment': r['loan_payment'],
+        'property_tax': r['property_tax'],
+        'hazard_insurance': r['hazard_insurance'],
+        'flood_insurance': r['flood_insurance'],
+        'utilities': r['utilities'],
+        'vacancy': r['vacancy'],
+        'repairs': r['repairs'],
+        'capex': r['capex'],
+        'management': r['management'],
+        'hoa': r['hoa'],
+        'misc_expense': r['misc_expense'],
+        'expenses_total': r['expenses_total'],
+        'cash_flow': r['cash_flow'],
+        'cash_flow_per_unit': r['cash_flow_per_unit'],
+        'net_operating_income': r['net_operating_income'],
+    }
+
+    for k, v in currency.iteritems():
+        r[k] = float_to_currency(v)
+
+
+    percents = {
+        'cap_rate': r['cap_rate'],
+        'cash_on_cash_return': r['cash_on_cash_return'],
+    }
+    for k, v in percents.iteritems():
+        r[k] = float_to_percent(v)
+
+
+    dollars = {
+        'purchase_price': r['purchase_price'],
+        'closing_cost': r['closing_cost'],
+        'repair_cost': r['repair_cost'],
+        'project_cost': r['project_cost'],
+    }
+    for k, v in dollars.iteritems():
+        r[k] = integer_to_currency(v)
+
+    # if value is 0, show a dash line '-' instead for readability
+    for k, v in currency.iteritems():
+        try:
+            f = float(v)
+            if f == 0.0:
+                r[k] = '-'
+        except:
+            pass
+
+
+
+
+    return r
+
+
+
 
 
 
@@ -88,6 +192,7 @@ if __name__ == '__main__':
         'loan_period_years': 30,
         'units_in_property': 2,
         'rental_income_monthly': 2000,
+        'misc_income_monthly': 0,
         'property_tax_annual': 5000,
         'hazard_insurance_annual': 1200,
         'flood_insurance_annual': 0,
@@ -97,9 +202,9 @@ if __name__ == '__main__':
         'capex_monthly_percent': 8,
         'manager_monthly_percent': 8,
         'hoa_monthly': 0,
-        'misc_monthly': 0,
+        'misc_expense_monthly': 0,
     }
-    calc_rental(data)
+    rental_main(data)
 
 
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import request, render_template, redirect, url_for, flash, abort, make_response, session, send_from_directory
 from forms import RentalIncomeCalculatorForm
-from rentmath import calc_rental
+from rentmath import *
 
 
 @app.errorhandler(404)
@@ -15,11 +15,52 @@ def index():
     return render_template('index.html', form=form)
 
 
-@app.route('/', methods=['POST'])
-def index_post():
+# @app.route('/', methods=['POST'])
+# def index_post():
+#     form = RentalIncomeCalculatorForm()
+#     if form.validate_on_submit():
+#         data = {
+#             'property_name': form.property_name.data,
+#             'purchase_price': form.purchase_price.data,
+#             'closing_cost': form.closing_cost.data,
+#             'repair_cost': form.repair_cost.data,
+#             'down_payment_percent': form.down_payment_percent.data,
+#             'loan_interest_rate_percent': form.loan_interest_rate_percent.data,
+#             'loan_period_years': form.loan_period_years.data,
+#             'units_in_property': form.units_in_property.data,
+#             'rental_income_monthly': form.rental_income_monthly.data,
+#             'misc_income_monthly': form.misc_income_monthly.data,
+#             'property_tax_annual': form.property_tax_annual.data,
+#             'hazard_insurance_annual': form.hazard_insurance_annual.data,
+#             'flood_insurance_annual': form.flood_insurance_annual.data,
+#             'utilities_monthly_percent': form.utilities_monthly_percent.data,
+#             'vacancy_monthly_percent': form.vacancy_monthly_percent.data,
+#             'repairs_monthly_percent': form.repairs_monthly_percent.data,
+#             'capex_monthly_percent': form.capex_monthly_percent.data,
+#             'manager_monthly_percent': form.manager_monthly_percent.data,
+#             'hoa_monthly': form.hoa_monthly.data,
+#             'misc_expense_monthly': form.misc_expense_monthly.data,
+#         }
+#         result = calc_rental(data)
+#
+#         return render_template('rental-calculator.html', result=result)
+#     return render_template('index.html', form=form)
+
+
+@app.route('/rental', methods=['GET'])
+def rental():
+    form = RentalIncomeCalculatorForm()
+    pass
+
+
+
+
+@app.route('/rental', methods=['POST'])
+def rental_post():
     form = RentalIncomeCalculatorForm()
     if form.validate_on_submit():
         data = {
+            'property_name': form.property_name.data,
             'purchase_price': form.purchase_price.data,
             'closing_cost': form.closing_cost.data,
             'repair_cost': form.repair_cost.data,
@@ -28,6 +69,7 @@ def index_post():
             'loan_period_years': form.loan_period_years.data,
             'units_in_property': form.units_in_property.data,
             'rental_income_monthly': form.rental_income_monthly.data,
+            'misc_income_monthly': form.misc_income_monthly.data,
             'property_tax_annual': form.property_tax_annual.data,
             'hazard_insurance_annual': form.hazard_insurance_annual.data,
             'flood_insurance_annual': form.flood_insurance_annual.data,
@@ -37,14 +79,12 @@ def index_post():
             'capex_monthly_percent': form.capex_monthly_percent.data,
             'manager_monthly_percent': form.manager_monthly_percent.data,
             'hoa_monthly': form.hoa_monthly.data,
+            'misc_expense_monthly': form.misc_expense_monthly.data,
         }
-        result = calc_rental(data)
+        result = rental_main(data)
 
-        return render_template('rental_income_calc.html', result=result)
-    return render_template('index.html', form=form)
-
-
-
+        return render_template('rental-calculator.html', result=result)
+    return redirect(url_for('index', form=form))
 
 
 
